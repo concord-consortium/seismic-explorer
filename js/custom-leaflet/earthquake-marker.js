b8474b636e9e0d90859abeffe9aec0e1737d3578
Piotr Janik <janikpiotrek@gmail.com>
2016-03-21 17:10:10 +0100

    Make earthquake marker work with Leaflet Canvas renderer
    [#111345110]
    
    There are still issues with Canvas renderer anyway.

--- js/custom-leaflet/earthquake-marker.js
+++ js/custom-leaflet/earthquake-marker.js
@@ -29,21 +29,34 @@ const EarthquakeMarker = CircleMarker.extend({
   setVisible: function (v) {
     if (this._visible === undefined) {
       this._visible = true
-      this._container.setAttribute('class', 'earthquake-marker')
+      this._setVis(true)
     }
-    if (!this._visible && v) {
-      this._container.setAttribute('class', 'earthquake-marker')
-    } else if (this._visible && !v) {
-      this._container.setAttribute('class', 'earthquake-marker hidden')
+    if (this._visible !== v) {
+      // Update element only if it's really needed.
+      this._setVis(v)
     }
     this._visible = v
+  },
+
+  _setVis: function (v) {
+    if (this._container) {
+      // SVG
+      this._container.setAttribute('class', `earthquake-marker${v ? '' : ' hidden'}`)
+    } else {
+      // Canvas
+      this.setStyle({opacity: v ? 0.2 : 0, fillOpacity: v ? 0.8 : 0})
+    }
   }
 })
 
 export default function earthquakeMarker(earthquake) {
   const marker = new EarthquakeMarker(earthquake.geometry.coordinates, {
     radius: magnitudeToRadius(earthquake.properties.mag),
-    fillColor: depthToColor(earthquake.geometry.coordinates[2])
+    fillColor: depthToColor(earthquake.geometry.coordinates[2]),
+    color: '#000',
+    weight: 1.5,
+    opacity: 0.2,
+    fillOpacity: 0.8
   })
   marker.bindPopup(popup(earthquake))
   return marker
