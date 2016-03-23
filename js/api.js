5bbf5b1d4d97c31d34d1144bfac973af48650bce
Piotr Janik <janikpiotrek@gmail.com>
2016-03-23 15:21:54 +0100

    Show data loading indicator
    [#115420199]
    
    + cache JSONs

--- js/api.js
+++ js/api.js
@@ -1,9 +1,38 @@
 import fetch from 'isomorphic-fetch'
 
+class Cache {
+  constructor() {
+    this.data = {}
+  }
+
+  // Use JSON.stringify and JSON.parse to ensure that data is copied and can't be mutated by client code.
+  get(path) {
+    return JSON.parse(this.data[path])
+  }
+
+  set(path, json) {
+    this.data[path] = JSON.stringify(json)
+    return json
+  }
+
+  has(path) {
+    return !!this.data[path]
+  }
+}
+
+
+const cache = new Cache()
+
 export function fetchJSON(path) {
+  if (cache.has(path)) {
+    return new Promise(resolve => {
+      resolve(cache.get(path))
+    })
+  }
   return fetch(path)
     .then(checkStatus)
     .then(response => response.json())
+    .then(response => cache.set(path, response))
 }
 
 export class APIError {
--- js/containers/app.js
+++ js/containers/app.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react'
 import pureRender from 'pure-render-decorator'
 import { connect } from 'react-redux'
 import * as actions from '../actions'
-import SeismicEruptionsMap from '../components/seismic-eruptions-map'
 import Controls from './controls'
+import SeismicEruptionsMap from '../components/seismic-eruptions-map'
+import LoadingIcon from '../components/loading-icon'
 
 import '../../css/app.less'
 
@@ -27,9 +28,10 @@ class App extends Component {
   }
 
   render() {
-    const { region, earthquakes, layers, animationEnabled } = this.props
+    const { region, earthquakes, layers, animationEnabled, dataFetching } = this.props
     return (
       <div className='seismic-eruptions-app'>
+        {dataFetching && <LoadingIcon/>}
         /* 'with-animation' class enables fancy animation of earthquakes when they are hidden or show.
            Enable it only when user started animation using play button, as it's too slow for manual
            filtering using sliders.  */
@@ -46,6 +48,7 @@ class App extends Component {
 
 function mapStateToProps(state) {
   return {
+    dataFetching: state.getIn(['dataStatus', 'isFetching']),
     filters: state.get('filters'),
     region: state.get('region'),
     layers: state.get('layers'),
