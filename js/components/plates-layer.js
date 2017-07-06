f481f19f83bf7ae46dea95e106bbf6d4b406dab5
FAFNIR\Saul <saulamster@gmail.com>
2017-07-06 10:25:09 -0400

    plates-layer.js now has options for KML and json files. New node package: leaflet-ajax Current map data has no visible style

--- js/components/plates-layer.js
+++ js/components/plates-layer.js
@@ -3,21 +3,32 @@ import pureRender from 'pure-render-decorator'
 import { MapLayer } from 'react-leaflet'
 // Import plugin using imports-loader.
 import 'imports?L=leaflet!leaflet-plugins/layer/vector/KML'
+import 'imports?L=leaflet!leaflet-ajax'
 import L from 'leaflet'
 
 let _cachedKML
 function getKML() {
   if (!_cachedKML) {
-    _cachedKML = new L.KML('plates.kml', {async: true})
+    _cachedKML = new L.KML('ComplexSeismicConverted.kml', {async: true})
   }
   return _cachedKML
+}
+
+let _cachedGeoJSON
+function getGeoJSON()
+{
+  if(!_cachedGeoJSON)
+  {
+    _cachedGeoJSON = new L.GeoJSON.AJAX("ComplexSeismicConverted.json");
+  }
+  return _cachedGeoJSON
 }
 
 @pureRender
 export default class PlatesLayer extends MapLayer {
   componentWillMount() {
     super.componentWillMount();
-    this.leafletElement = getKML()
+    this.leafletElement = getGeoJSON();
   }
 
   render() {
