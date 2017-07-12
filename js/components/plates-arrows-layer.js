bc4f1a1c7287db964760d393e9a6be22c8a52705
Saul Amster <saulamster@gmail.com>
2017-07-12 16:28:13 -0400

    Added custom arrow marker creator

--- js/components/plates-arrows-layer.js
+++ js/components/plates-arrows-layer.js
@@ -4,10 +4,38 @@ import { MapLayer } from 'react-leaflet'
 import L from 'leaflet'
 import 'imports?L=leaflet!leaflet-plugins/layer/Marker.Rotate.js'
 
-
-function markerMaker()
+const arrowHeadToBodyRatio = 0.25
+const arrowAngleOffset = 0.2
+function markerMaker(posX, posY, angle, magnitude)
 {
-  return new L.Marker([0, 0], {iconAngle: 27})
+  var arrowHeadLength = magnitude * arrowHeadToBodyRatio
+  var trigLength = arrowHeadLength * Math.cos(angle)
+
+  var latlngs = [[posX, posY],
+                [posX + magnitude * Math.cos(angle),posY + magnitude * Math.sin(angle)],
+                [posX + (magnitude - arrowHeadLength) * Math.cos(angle + arrowAngleOffset), posY + (magnitude - arrowHeadLength) * Math.sin(angle + arrowAngleOffset)],
+                [posX + (magnitude - arrowHeadLength) * Math.cos(angle - arrowAngleOffset), posY + (magnitude - arrowHeadLength) * Math.sin(angle - arrowAngleOffset)],
+                [posX + magnitude * Math.cos(angle),posY + magnitude * Math.sin(angle)]]
+  var polyline = L.polyline(latlngs, {color: 'red', opacity:1});
+
+  return polyline
+}
+
+function dualArrowMaker(posX, posY, angle, magnitude)
+{
+  return new L.LayerGroup([
+    markerMaker(posX, posY, angle, magnitude),
+    markerMaker(posX, posY, angle, -1 * magnitude)
+  ])
+}
+
+function arrowLayerCreator()
+{
+  var layer = new L.LayerGroup([
+    dualArrowMaker(0, 0, Math.random() * 2 * Math.PI, Math.random() * 8 + 2),
+    dualArrowMaker(0, 0, Math.random() * 2 * Math.PI, Math.random() * 8 + 2),
+  ])
+  return layer
 }
 
 @pureRender
@@ -15,12 +43,12 @@ export class PlatesArrowsLayer extends MapLayer {
 
   constructor() {
     super();
-    this.leafletElement = markerMaker();
+    this.leafletElement = arrowLayerCreator()
   }
 
   componentWillMount() {
     super.componentWillMount();
-    this.leafletElement = markerMaker();
+    this.leafletElement =arrowLayerCreator()
   }
 
   render() {
