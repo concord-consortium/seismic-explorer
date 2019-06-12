ca8a51e4f967b32a3d7d5780f2bac11d0acdbbc7
pjanik <janikpiotrek@gmail.com>
2019-06-12 15:51:18 +0200

    Update about content
    [#157705491]

--- js/components/about-modal-content.js
+++ js/components/about-modal-content.js
@@ -22,7 +22,11 @@ export default class AboutModalContent extends PureComponent {
             href='http://serc.carleton.edu/resources/23210.html' target='_blank'>Alan L. Jones at the State University
             of New York at Binghamton</a>.</p>
           <p>Seismic Explorer uses earthquake data (magnitude, depth, location, time) from the <a
-            href='http://earthquake.usgs.gov/earthquakes/map/' target='_blank'>United States Geological Survey</a>.</p>
+            href='http://earthquake.usgs.gov/earthquakes/map/' target='_blank'>United States Geological Survey</a>.
+            Earthquake time is reported in Coordinated Universal Time (UTC).</p>
+          <p>Volcano data (location, last eruption date) comes from the <a
+            href='https://volcano.si.edu/reports_weekly.cfm' target='_blank'>Smithsonian Institution Global Volcanism Program</a>.
+          </p>
           <p>Street map images are hosted by <a href='https://foundation.wikimedia.org/w/index.php?title=Maps_Terms_of_Use#Where_does_the_map_data_come_from.3F' target='_blank'>The Wikimedia Foundation</a>, serving map data from <a href='https://openstreetmap.org' target='_blank'>&#169;OpenStreetMap</a>.</p>
         </div>
       </div>
