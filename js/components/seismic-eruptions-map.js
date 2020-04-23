1ed29a69696a94f80897648fccef2c0bf9cf6919
Chris Hart <chart@concord.org>
2020-04-23 16:00:22 -0400

    Fix parameter name

--- js/components/seismic-eruptions-map.js
+++ js/components/seismic-eruptions-map.js
@@ -251,7 +251,7 @@ export default class SeismicEruptionsMap extends PureComponent {
     const center = config.sizeToFitBounds ? undefined : INITIAL_CENTER
     const zoom = this.map ? this.mapZoom : config.centeredInitialZoom
 
-    const showZoomControls = config.minZoom !== config.maxZoom
+    const showZoomControls = config.zoomMin !== config.zoomMax
 
     return (
       <div className={`seismic-eruptions-map mode-${mode}`}>
