087d9b9d7c40bf377c952bda4d2731de2ba9d0f6
Chris Hart <chart@concord.org>
2020-11-09 16:42:43 -0500

    Mirror changes from starter-projects

--- deploy/s3_deploy.sh
+++ deploy/s3_deploy.sh
@@ -1,26 +1,65 @@
-#!/bin/sh
 
-# this will deploy the current public folder to a subfolder in the s3 bucket
-# the subfolder is the name of the TRAVIS_BRANCH
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-	echo "skiping deploy to S3: this is a pull request"
-	exit 0
-fi
+#!/bin/bash
+SRC_DIR='dist'
+DISTRIBUTION_ID='E3MT1G8AEEAXO'
+# name of branch to deploy to root of site
+PRODUCTION_BRANCH='production'
 
 if [ "$TRAVIS_BRANCH" = "production" ]; then
   mv dist _site
 else
   # the 2> is to prevent error messages when no match is found
-  CURRENT_TAG=`git describe --tags --exact-match $TRAVIS_COMMIT 2> /dev/null`
-  if [ "$TRAVIS_BRANCH" = "$CURRENT_TAG" ]; then
-    # this is a tag build
-    mkdir -p _site/version
-    DEPLOY_DIR=version/$TRAVIS_BRANCH
-  else
-    mkdir -p _site/branch
-    DEPLOY_DIR=branch/$TRAVIS_BRANCH
-  fi
-  mv dist _site/$DEPLOY_DIR
-  export DEPLOY_DIR
+CURRENT_TAG=`git describe --tags --exact-match $GITHUB_SHA 2> /dev/null`
+
+# Extract the branch or tag name from the GITHUB_REF
+# it should either be: refs/head/branch-name or
+# or refs/tags/v1.2.3
+# since we ought to know if this is a branch or tag based on the ref
+# we could simplify the CURRENT_TAG approach above
+BRANCH_OR_TAG=${GITHUB_REF#refs/*/}
+echo branch or tag: $BRANCH_OR_TAG
+
+# strip PT ID from branch name for branch builds
+DEPLOY_DIR_NAME=$BRANCH_OR_TAG
+PT_PREFIX_REGEX="^([0-9]{8,}-)(.+)$"
+PT_SUFFIX_REGEX="^(.+)(-[0-9]{8,})$"
+if [[ $DEPLOY_DIR_NAME =~ $PT_PREFIX_REGEX ]]; then
+  DEPLOY_DIR_NAME=${BASH_REMATCH[2]}
 fi
-s3_website push --site _site --config-dir deploy
+if [[ $DEPLOY_DIR_NAME =~ $PT_SUFFIX_REGEX ]]; then
+  DEPLOY_DIR_NAME=${BASH_REMATCH[1]}
+fi
+
+# tagged builds deploy to /version/TAG_NAME
+if [ "$BRANCH_OR_TAG" = "$CURRENT_TAG" ]; then
+  mkdir -p _site/version
+  S3_DEPLOY_DIR="version/$BRANCH_OR_TAG"
+  DEPLOY_DEST="_site/$S3_DEPLOY_DIR"
+  INVAL_PATH="/version/$BRANCH_OR_TAG/index.html"
+  # used by s3_website.yml
+  export S3_DEPLOY_DIR
+
+# production branch builds deploy to root of site
+elif [ "$BRANCH_OR_TAG" = "$PRODUCTION_BRANCH" ]; then
+  DEPLOY_DEST="_site"
+  INVAL_PATH="/index.html"
+
+# branch builds deploy to /branch/BRANCH_NAME
+else
+  mkdir -p _site/branch
+  S3_DEPLOY_DIR="branch/$DEPLOY_DIR_NAME"
+  DEPLOY_DEST="_site/$S3_DEPLOY_DIR"
+  INVAL_PATH="/branch/$DEPLOY_DIR_NAME/index.html"
+  # used by s3_website.yml
+  export S3_DEPLOY_DIR
+fi
+
+# copy files to destination
+mv $SRC_DIR $DEPLOY_DEST
+
+# deploy the site contents
+s3_website push --site _site
+
+# explicit CloudFront invalidation to workaround s3_website gem invalidation bug
+# with origin path (https://github.com/laurilehmijoki/s3_website/issues/207).
+aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths $INVAL_PATH
