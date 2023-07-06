70af086043585c2131c7f06556fa74d85189a93c
pjanik <janikpiotrek@gmail.com>
2023-07-06 17:50:23 +0200

    chore: switch to new release action [PT-185545046]

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
 var path = require('path')
 var webpack = require('webpack')
 var CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// DEPLOY_PATH is set by the s3-deploy-action its value will be:
+// `branch/[branch-name]/` or `version/[tag-name]/`
+// See the following documentation for more detail:
+// https://github.com/concord-consortium/s3-deploy-action/blob/main/README.md#top-branch-example
+const DEPLOY_PATH = process.env.DEPLOY_PATH;
 
 module.exports = {
   mode: process.env.PRODUCTION ? 'production' : 'development',
@@ -88,6 +95,17 @@ module.exports = {
     ]
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'js/index.html',
+      favicon: 'public/favicon.ico'
+    }),
+    ...(DEPLOY_PATH ? [new HtmlWebpackPlugin({
+      filename: 'index-top.html',
+      template: 'js/index.html',
+      favicon: 'public/favicon.ico',
+      publicPath: DEPLOY_PATH
+    })] : []),
     new CopyWebpackPlugin({
       patterns: [
         { from: 'public' }
